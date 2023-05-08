d8cdbf4358b31ae9be0fade3ae5368ee6c623063
Raptorij <i.raptorij@gmail.com>
2023-05-08 21:25:32 +0700

    Add String Extention: FirstCharToUpper

--- StringExtensions.cs
+++ StringExtensions.cs
@@ -127,4 +127,12 @@ public static class StringExtensions
         }
         return result.ToArray();
     }
+
+    public static string FirstCharToUpper(this string input) =>
+        input switch
+        {
+            null => throw new System.ArgumentNullException(nameof(input)),
+            "" => throw new System.ArgumentException($"{nameof(input)} cannot be empty", nameof(input)),
+            _ => input[0].ToString().ToUpper() + input.Substring(1)
+        };
 }
